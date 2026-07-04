70c2d684b08d6cf657e8200dc5757481fea10dd2
Yash Palan <ypalan@gmail.com>
2026-07-04 22:50:33 +0530

    new file:   class_defns/classical_head_classes.py

---
+++ class_defns/classical_head_classes.py
@@ -0,0 +1,48 @@
+import torch
+from torch import nn 
+
+class neural_net(nn.Module):
+    """
+    Defines a class for creating a dynamic neural net which can be tuned based on ones preference.
+    """
+    def __init__(self, layer_geometry:torch.Tensor, activation_functions:nn.ModuleList ):
+        """
+        Layer geometry is a 
+        """
+        super().__init__()
+
+        # Makes checks about the inputs
+        self.checks(layer_geometry,activation_functions)
+        
+        # Define the layer geometry in a modulelist
+        self.layers = nn.ModuleList()
+        for i in range(1,layer_geometry.shape[0]):
+            self.layers.append(nn.Linear(in_features=layer_geometry[i-1],out_features=layer_geometry[i])) 
+        
+        # Define the activation function geometry
+        self.activation_function = activation_functions
+        return
+    
+    def checks(self,layer_geometry,activation_functions):
+        
+        if layer_geometry.dtype !=torch.int :
+            raise Exception("wrong dtype for layer_geometry")
+        
+        # Checks if both have same length
+        if((len(layer_geometry)-1)!= len(activation_functions)):
+            raise Exception("The length of the layer_geometry and activation_functions is not same")
+        
+
+    def forward(self,input_vector:torch.Tensor):
+        x = input_vector
+        for i in range(len(self.layers)):
+            x = self.activation_function[i](self.layers[i](x))
+        return(x)
+
+if __name__=="__main__":
+    activation_functions = nn.ModuleList([nn.ReLU(),nn.Tanh()])
+    layer_geometry = torch.tensor([4,64,2],dtype=torch.int)
+    nn_object = neural_net(layer_geometry,activation_functions=activation_functions)
+    print(nn_object)
+    print(nn_object.forward(torch.tensor([[1,2,3,4]],dtype=torch.float32)))
+    # print(nn_object.)
